25dc7c6cda8a861eecfe6ea286af2c37feef0497
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2025-03-10 11:23:32 -0700

    new steps

---
+++ dither_20250210.sh
@@ -0,0 +1,3 @@
+source /global/common/software/desi/desi_environment.sh main
+
+./fba_cmx_new --dr dr9 --dtver 0.49.0 --rundate 2025-03-10T10:00:00+00:00 --seed 77  --tilera 120.0  --tiledec -30.0 --tileid 82853  --faflavor dithprec  --outdir ./
