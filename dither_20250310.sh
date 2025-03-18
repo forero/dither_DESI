80561ccec071485502b9966e25a6591c800c5f05
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2025-03-18 11:27:25 -0700

    actual dither script

--- dither_20250310.sh
+++ dither_20250310.sh
@@ -1,3 +1,5 @@
 source /global/common/software/desi/desi_environment.sh main
 
-./fba_cmx_new --dr dr9 --dtver 0.49.0 --rundate 2025-03-10T10:00:00+00:00 --seed 77  --tilera 120.0  --tiledec -30.0 --tileid 82853  --faflavor dithprec  --outdir ./
+./fba_cmx_new --dr dr9 --dtver 0.49.0 --rundate 2025-03-10T10:00:00+00:00 --seed 77  --tilera 160.0  --tiledec -30.0 --tileid 82853  --faflavor dithprec  --outdir ./
+./fba_cmx_new --dr dr9 --dtver 0.49.0 --rundate 2025-03-10T10:00:00+00:00 --seed 77  --tilera 190.0  --tiledec -30.0 --tileid 82866  --faflavor dithprec  --outdir ./
+./fba_cmx_new --dr dr9 --dtver 0.49.0 --rundate 2025-03-10T10:00:00+00:00 --seed 77  --tilera 220.0  --tiledec -30.0 --tileid 82879  --faflavor dithprec  --outdir ./
