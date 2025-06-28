8d902772ddaa58df5b70ba60e86baa47749c5a62
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2025-06-28 16:31:11 -0700

    latest dither designs

---
+++ dither_20250628.sh
@@ -0,0 +1,5 @@
+source /global/common/software/desi/desi_environment.sh main
+
+./fba_cmx_new --dr dr9 --dtver 0.49.0 --rundate 2025-06-28T10:00:00+00:00 --seed 77  --tilera 280.0  --tiledec -30.0 --tileid 82892  --faflavor dithprec  --outdir ./
+./fba_cmx_new --dr dr9 --dtver 0.49.0 --rundate 2025-06-28T10:00:00+00:00 --seed 77  --tilera 300.0  --tiledec -30.0 --tileid 82905  --faflavor dithprec  --outdir ./
+./fba_cmx_new --dr dr9 --dtver 0.49.0 --rundate 2025-06-28T10:00:00+00:00 --seed 77  --tilera 330.0  --tiledec -30.0 --tileid 82918  --faflavor dithprec  --outdir ./
