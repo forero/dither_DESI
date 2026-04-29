46d5c15d9bc7ff1df1af00e5be258426250b686d
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2026-04-28 17:15:46 -0700

    new dither tiles

---
+++ dither_20260428.sh
@@ -0,0 +1,4 @@
+source /global/common/software/desi/desi_environment.sh main
+
+./fba_cmx_new --dr dr9 --dtver 0.49.0 --rundate 2026-04-28T10:00:00+00:00 --seed 80  --tilera 269  --tiledec -25.0 --tileid 84078  --faflavor dithprec  --outdir ./
+
