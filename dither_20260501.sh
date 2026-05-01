10099387ef434aeb0382176c34c5f06b62f05ce2
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2026-05-01 08:43:08 -0700

    new dither to replace old sequence

---
+++ dither_20260501.sh
@@ -0,0 +1,4 @@
+source /global/common/software/desi/desi_environment.sh main
+
+./fba_main_dither --dr dr9 --dtver 1.0.0 --rundate 2026-04-29T10:00:00+00:00 --seed 80  --tilera 175  --tiledec -25.0 --tileid 84078  --faflavor dithprec  --outdir ./
+
